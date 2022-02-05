206644f5d6f2db978def64f470fb47927141456a
Devika Manoj <dmenon138@gmail.com>
2022-02-06 00:40:23 +0530

    Users added

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,72 +1,132 @@
 import java.util.*;
-
 public class Sudoku
 {
-
+    static int[][] Sudoku=new int[9][9];
+    static int[][] Sudoku_New=new int[9][9];
+    static HashMap <Integer,String> Username= new HashMap<Integer,String>();
+    static HashMap <Integer,String> Password= new HashMap<Integer,String>();
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) 
     {
-        Scanner in = new Scanner(System.in);
-        System.out.println("        SUDOKO");
-        System.out.println("        ------");
-        int[][] Sudoku=new int[9][9];
-        Sudoku_initialise.initialise(Sudoku);
+
+        System.out.println("        ************************");
+        System.out.println("        WELCOME TO SUDOKO SOLVER");
+        System.out.println("        ************************");
+
+        System.out.println(" Are you a Admin or a Player");
+        System.out.println(" 1. Admin \n 2. Player");
+
+        int choice=in.nextInt();
+        if(choice==1)
+        {
+            adminLogin();
+        }
+        else if(choice==2)
+        {
+            UserLogin();
+        }
+        else
+        {
+            System.out.println(" INVALID CHOICE !!!");
+        }
+
+        in.close();
+    }
+    static void adminLogin()
+    {
+        System.out.println(" Are you a New User or an Old User ?");
+        System.out.println(" 1. New User \n 2. Old User");
+        int choice =in.nextInt();
+
+        if(choice ==1)
+        {
+            NewUser();
+        }
+        else if(choice ==2)
+        {
+            OldUser();
+        }
+        else
+        {
+            System.out.println(" INVALID CHOICE !!!");
+        }
+    }
+    static void OldUser()
+    {
+        System.out.println();
+        System.out.println(" Enter Username: ");
+        String user=in.next();
+        System.out.println(" Enter Password: ");
+        String pass=in.next();
+        checkUserPresent(user, pass);
+    }
+    static void checkUserPresent(String user,String Pass)
+    {
+        String name,pass;
+        for(int i=0;i<Username.size();i++)
+        {
+            name = Username.get(i);
+            if(name.equals(user))
+            {
+                pass = Password.get(i);
+                if(Pass.equals(pass))
+                {
+                    wait(2000);
+                    System.out.println(" LOGGED IN");
+                    wait(1000);
+                    UserLogin();
+                }
+            }
+        }
+    }
+    static void NewUser()
+    {
+
+    }
+    public static void wait(int ms)
+	{
+		try
+		{
+			Thread.sleep(ms);
+		}
+		catch(InterruptedException ex)
+		{
+			Thread.currentThread().interrupt();
+		}
+	}
+    static void UserLogin()
+    {
         int row,column,number;
+        
         System.out.print("Enter the row: ");
         row=in.nextInt();
         System.out.print("\n Enter the column: ");
         column=in.nextInt();
 
-        // checks if the above mentioned position is occupied or not
-        if(Sudoku[row-1][column-1]!=0)
+        while(Sudoku_Methods.ElementZero(Sudoku))
         {
-            System.out.println(" Oops !!! The given position is occupied !!!");
-            number=Sudoku[row-1][column-1];
-        }
-        else
-        {
-            System.out.print("\n Enter the number: ");
-            number=in.nextInt();
+            // checks if the above mentioned position is occupied or not
+            if(Sudoku[row-1][column-1]!=0)
+            {
+                System.out.println(" Oops !!! The given position is occupied !!!");
+                number=Sudoku[row-1][column-1];
+            }
+            else
+            {
+                System.out.print("\n Enter the number: ");
+                number=in.nextInt();
+            }
+
+            if(Sudoku[row-1][column-1]==0 &&!Sudoku_Methods.IsRepeatingCol(Sudoku, number) && !Sudoku_Methods.IsRepeatingRow(Sudoku, number))
+            {
+                Sudoku[row-1][column-1]=number;
+            }
+            else
+            {
+                System.out.println(" Oops !!! The number has been repeated !!!");
+            }
         }
 
-        if(Sudoku[row-1][column-1]==0 &&!IsRepeatingCol(Sudoku, number) && !IsRepeatingRow(Sudoku, number))
-        {
-            Sudoku[row-1][column-1]=number;
-        }
-        else
-        {
-            System.out.println(" Oops !!! The number has been repeated !!!");
-        }
         Sudoku_initialise.Print(Sudoku);
-        in.close();
-    }
-    static boolean IsRepeatingRow(int [][] Sudoku,int number)
-    {
-        for(int i=0;i<9;i++)
-        {
-            int row=i;
-            for(int j=0;j<9;j++)
-            {
-                if(Sudoku[row][j]==number)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    static boolean IsRepeatingCol(int [][] Sudoku,int number)
-    {
-        for(int i=0;i<9;i++)
-        {
-            int col=i;
-            for(int j=0;j<9;j++)
-            {
-                if(Sudoku[j][col]==number)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
---
+++ src/Sudoku_Methods.java
@@ -0,0 +1,47 @@
+public class Sudoku_Methods
+{
+    static boolean ElementZero(int [][] Sudoku)
+    {
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(Sudoku[i][j]==0)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static boolean IsRepeatingRow(int [][] Sudoku,int number)
+    {
+        for(int i=0;i<9;i++)
+        {
+            int row=i;
+            for(int j=0;j<9;j++)
+            {
+                if(Sudoku[row][j]==number)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static boolean IsRepeatingCol(int [][] Sudoku,int number)
+    {
+        for(int i=0;i<9;i++)
+        {
+            int col=i;
+            for(int j=0;j<9;j++)
+            {
+                if(Sudoku[j][col]==number)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
