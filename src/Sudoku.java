5f9deafd5d61d753a3005c6cbfb7fa6a2c84135c
Devika Manoj <dmenon138@gmail.com>
2022-02-05 21:49:10 +0530

    Printing corrected

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -9,45 +9,10 @@ public class Sudoku
         System.out.println("        SUDOKO");
         System.out.println("        ------");
         int[][] Sudoku=new int[9][9];
-        
-        for(int i=0;i<9;i++)
-        {
-            System.out.print("Enter element to row "+(i+1)+": ");
-            for(int j=0;j<9;j++)
-            {
-                Sudoku[i][j]=in.nextInt();
-            }
-        }
+        Sudoku_initialise.initialise(Sudoku);
+        Sudoku_initialise.Print(Sudoku);
 
-    }
-    static void initialise(int [][]Sudoko)
-    {
-        // row 1
-        Sudoko[0][1]=1 ;Sudoko[0][2]=2 ; Sudoko[0][8]=5 ;
-
-        //row 2
-        Sudoko[1][5]=6 ;Sudoko[1][6]=1 ;Sudoko[1][7]=8 ;
-
-        //row 3
-        Sudoko[2][2]=4 ;Sudoko[2][4]=3 ;Sudoko[2][7]=7 ;
-
-        //row 4
-        Sudoko[3][1]=2 ;Sudoko[3][4]=8 ;Sudoko[3][7]=4 ;
-
-        //row 5
-        Sudoko[4][5]=6 ;Sudoko[4][5]=6 ;Sudoko[4][5]=6 ;Sudoko[4][5]=6 ;
-
-        //row 6
-        Sudoko[5][5]=6 ; Sudoko[5][5]=6 ; Sudoko[5][5]=6 ; Sudoko[5][5]=6 ;
-
-        //row 7
-        Sudoko[6][5]=6 ;Sudoko[6][5]=6 ;Sudoko[6][5]=6 ;
-
-        //row 8
-        Sudoko[7][5]=6 ;Sudoko[7][5]=6 ;Sudoko[7][5]=6 ;Sudoko[7][5]=6 ;
-        
-        //row 9
-        Sudoko[8][5]=6 ;Sudoko[8][5]=6 ;Sudoko[8][5]=6 ;Sudoko[8][5]=6 ;
+        in.close();
     }
     static boolean IsRepeatingRow(int [][] Sudoku,int number)
     {
@@ -78,16 +43,5 @@ public class Sudoku
             }
         }
         return false;
-    }
-    static void Print(int[][] Sudoku)
-    {
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                System.out.print(Sudoku[i][j]+" ");
-            }
-            System.out.println();
-        }
     }
 }
---
+++ src/Sudoku_initialise.java
@@ -0,0 +1,65 @@
+public class Sudoku_initialise 
+{
+    
+    static void initialise(int [][]Sudoku)
+    {
+        // row 1
+        Sudoku[0][1]=1 ;Sudoku[0][2]=2 ; Sudoku[0][8]=5 ;
+
+        //row 2
+        Sudoku[1][5]=6 ;Sudoku[1][6]=1 ;Sudoku[1][7]=8 ;
+
+        //row 3
+        Sudoku[2][2]=4 ;Sudoku[2][4]=3 ;Sudoku[2][7]=7 ;
+
+        //row 4
+        Sudoku[3][1]=2 ;Sudoku[3][4]=8 ;Sudoku[3][7]=4 ;
+
+        //row 5
+        Sudoku[4][0]=9 ;Sudoku[4][1]=7 ;Sudoku[4][6]=3 ;Sudoku[4][7]=6 ;
+
+        //row 6
+        Sudoku[5][2]=6 ; Sudoku[5][4]=1 ; Sudoku[5][5]=9 ; Sudoku[5][6]=5 ;
+
+        //row 7
+        Sudoku[6][1]=9 ;Sudoku[6][3]=4 ;Sudoku[6][4]=7 ;
+
+        //row 8
+        Sudoku[7][0]=8 ;Sudoku[7][1]=5 ;Sudoku[7][6]=2 ;Sudoku[7][8]=4 ;
+        
+        //row 9
+        Sudoku[8][2]=3 ;Sudoku[8][4]=9 ;Sudoku[8][5]=2 ;Sudoku[8][6]=8 ;
+    }
+    
+    static void Print(int[][] Sudoku)
+    {
+        System.out.println(" ---------------------------------");
+        // for box 1
+        for(int i=0;i<9;i++)
+        {
+            int col=i;
+            if(i==3||i==6)
+            {
+                System.out.println(" ---------------------------------");
+            }
+            for(int j=0;j<9;j++)
+            {
+                if(j==0)
+                {
+                    System.out.print(" | "+Sudoku[j][col]+" ");
+                }
+                else if(j==2 ||j==5||j==8)
+                {
+                    System.out.print(" "+Sudoku[j][col]+" | ");
+                }
+
+                else
+                {
+                    System.out.print(" "+Sudoku[j][col]+" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(" ---------------------------------");
+    }
+}
