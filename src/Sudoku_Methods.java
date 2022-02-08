9ab0e244e324ebc502ba828dc00acb6d6a1fa514
Devika Manoj <dmenon138@gmail.com>
2022-02-08 17:39:46 +0530

    Checking methods added

--- src/Sudoku_Methods.java
+++ src/Sudoku_Methods.java
@@ -1,48 +1,39 @@
+import java.util.*;
 public class Sudoku_Methods
 {
     static int[][] Sudoku=new int[9][9];
     static int[][] Sudoku_New=new int[9][9];
+    static Scanner in = new Scanner(System.in);
 
-    static boolean ElementZero(int [][] Sudoku)
+    static boolean ElementZero(int [][] Sudoku, int row,int col)
+    {
+        if(Sudoku[row][col]!=0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    static boolean IsRepeatingRow(int [][] Sudoku,int number,int row)
     {
         for(int i=0;i<9;i++)
         {
-            for(int j=0;j<9;j++)
+            if(Sudoku[row][i]==number)
             {
-                if(Sudoku[i][j]==0)
-                {
-                    return true;
-                }
+                return true;
             }
         }
         return false;
     }
-    static boolean IsRepeatingRow(int [][] Sudoku,int number)
+    static boolean IsRepeatingCol(int [][] Sudoku,int number,int col)
     {
         for(int i=0;i<9;i++)
         {
-            int row=i;
-            for(int j=0;j<9;j++)
+            if(Sudoku[i][col]==number)
             {
-                if(Sudoku[row][j]==number)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    static boolean IsRepeatingCol(int [][] Sudoku,int number)
-    {
-        for(int i=0;i<9;i++)
-        {
-            int col=i;
-            for(int j=0;j<9;j++)
-            {
-                if(Sudoku[j][col]==number)
-                {
-                    return true;
-                }
+                return true;
             }
         }
         return false;
@@ -87,6 +78,42 @@ public class Sudoku_Methods
     }
     static void ElementAdd()
     {
+        System.out.print("\n Enter the number: ");
+        int num = in.nextInt();
+        int row,col;
+        if(CorrectNum(num))
+        {
+            System.out.print("\n Enter the row: ");
+            row = in.nextInt();
+            System.out.print("\n Enter the column: ");
+            col = in.nextInt();
 
+            if(!IsRepeatingCol(Sudoku, num, col) && !IsRepeatingRow(Sudoku, num, row) && ElementZero(Sudoku, row, col) && !CheckMainElem(row, col))
+            {
+                Sudoku[row][col]=num;
+            }
+        }
+    }
+    static boolean CorrectNum(int num)
+    {
+        if(num>0 && num<=9)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static boolean CheckMainElem(int row, int col)
+    {
+        if(Sudoku_New[row][col]!=0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
